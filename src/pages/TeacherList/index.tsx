bb9c28697c9f5a27b631060a57275751ee88a553
Kamila-Vieira <vkamila54@outlook.com>
2020-08-18 13:14:42 -0300

    Favicon updated

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -62,12 +62,12 @@ function TeacherList(){
                         onChange={(e) => { setWeek_day(e.target.value)}} 
                         options={[
                             {value: '0', label: 'Domingo'},
-                            {value: '1', label: 'Segunda'},
-                            {value: '2', label: 'Terça'},
-                            {value: '3', label: 'Quarta'},
-                            {value: '4', label: 'Quinta'},
-                            {value: '5', label: 'Sexta'},
-                            {value: '6', label: 'Sábado'},
+                            {value: '1', label: 'Segunda-feira'},
+                            {value: '2', label: 'Terça-feira'},
+                            {value: '3', label: 'Quarta-feira'},
+                            {value: '4', label: 'Quinta-feira'},
+                            {value: '5', label: 'Sexta-feira'},
+                            {value: '6', label: 'Sábado-feira'},
                         ]}
                     />
 
